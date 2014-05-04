68695029d7933fc0272b4cd5d541ff9473db340b
Melissa Voegeli <melissa@code42.com>
2014-05-04 14:22:23 -0500

    1) change string and version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject awesome "0.1.0-SNAPSHOT"
+(defproject awesome "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
