a5eb7cb2c6753cda053c716bc740fe6fdce5f9de
Melissa Voegeli <melissa@code42.com>
2014-05-04 14:36:08 -0500

    2) add hiccup example

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler awesome.handler/app}
   :profiles
--- src/awesome/handler.clj
+++ src/awesome/handler.clj
@@ -1,10 +1,12 @@
 (ns awesome.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [hiccup.page :as page]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello My Name")
+  (GET "/" [] "HelloName")
+  (GET "/hello2" [] (page/html5 [:div "Hello from " [:b "hiccup"] "!"]))
   (route/resources "/")
   (route/not-found "Not Found"))
 
