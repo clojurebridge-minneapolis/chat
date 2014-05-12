83c41f9335fad59245f2ee055aaf50b583e88e99
Tom Marble <tmarble@info9.net>
2014-05-12 10:50:43 -0500

    Minor fixes for heroku, closes issue #2

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject awesome "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.3.4"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
+                 [javax.servlet/servlet-api "2.5"]
                  [hiccup "1.0.5"]
                  [hiccup-bootstrap "0.1.2"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler awesome.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
